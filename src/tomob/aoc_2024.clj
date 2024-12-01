14c780ab8fb445f1a0b19c4b8b5b940f54adc122
Tomasz Barański <tomasz.baranski@posteo.net>
2024-12-01 19:05:23 +0100

    Added timing to runner

--- src/tomob/aoc_2024.clj
+++ src/tomob/aoc_2024.clj
@@ -7,8 +7,8 @@
 (defn run-day [day]
   (let [step1 (requiring-resolve (step-symbol day "1"))
         step2 (requiring-resolve (step-symbol day "2"))]
-    (println "Step 1: " (step1))
-    (println "Step 2: " (step2))))
+    (println "Step 1: " (time (step1)))
+    (println "Step 2: " (time (step2)))))
 
 (defn -main
   "I don't do a whole lot ... yet."
