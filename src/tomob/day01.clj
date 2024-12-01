f4bcf8872593f3fd264ff380d401678553cfe418
Tomasz Barański <tomasz.baranski@posteo.net>
2024-12-01 17:34:57 +0100

    Day 01 part 1

--- src/tomob/day01.clj
+++ src/tomob/day01.clj
@@ -1,7 +1,33 @@
-(ns tomob.day01)
+(ns tomob.day01
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]))
+
+(defn unzip 
+  ([list-of-pairs] (unzip list-of-pairs [] []))
+  ([list-of-pairs left right]
+   (if (empty? list-of-pairs)
+     [left right]
+     (unzip (rest list-of-pairs) 
+            (conj left (first (first list-of-pairs)))
+            (conj right (second (first list-of-pairs)))))))
+
+(defn zip [left right]
+  (map vector left right))
+
+(defn parse-input [input]
+  (unzip (for [line (doall input)]
+    (map #(Integer. %) (string/split line #" +")))))
 
 (defn step1 []
-  'not-implemented)
+  (let [[left right]
+          (with-open [data (io/reader (io/resource "day01/step1-data.txt"))]
+            (parse-input (line-seq data)))
+        s-left (sort left)
+        s-right (sort right)
+        zipped (zip s-left s-right)]
+    (->> zipped
+         (map (fn [[a b]] (abs (- a b))))
+         (apply +))))
 
 (defn step2 []
   'not-implemented)
