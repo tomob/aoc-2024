04281597f59a49f28a0c9ce30a13497787de308c
Tomasz Barański <tomasz.baranski@posteo.net>
2024-12-03 19:08:51 +0100

    Make runner smart about data files

--- src/tomob/aoc_2024.clj
+++ src/tomob/aoc_2024.clj
@@ -5,22 +5,25 @@
 (defn step-symbol [day step]
   (symbol (str "tomob." day "/step" step)))
 
-(defn get-data-file [day data]
+(defn select-file [day & files]
+  (->> files
+       (map #(io/resource (str day "/" %)))
+       (drop-while #(nil? (io/as-file %)))
+       first))
+
+(defn get-data-file [day step data]
   (case data
-    "data" (io/resource (str day "/data.txt"))
-    "example" (io/resource (str day "/example.txt"))
-    "step1-data" (io/resource (str day "/step1-data.txt"))
-    "step1-example" (io/resource (str day "/step1-example.txt"))
-    "step2-data" (io/resource (str day "/step2-data.txt"))
-    "step2-example" (io/resource (str day "/step2-example.txt"))
+    "data" (select-file day "data.txt" (str "step" step "-data.txt"))
+    "example" (select-file day "example.txt" (str "step" step "-example.txt"))
     (throw (RuntimeException. "Invalid data set"))))
 
 (defn run-day [day data]
   (let [step1 (requiring-resolve (step-symbol day "1"))
         step2 (requiring-resolve (step-symbol day "2"))
-        data (get-data-file day data)]
-    (println "Step 1: " (time (step1 data)))
-    (println "Step 2: " (time (step2 data)))))
+        data1 (get-data-file day 1 data)
+        data2 (get-data-file day 2 data)]
+    (println "Step 1: " (time (step1 data1)))
+    (println "Step 2: " (time (step2 data2)))))
 
 (defn -main
   "I don't do a whole lot ... yet."
