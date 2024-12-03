7d7b784aa9134eb95f0fdd5caf313ef9c9fcd98b
Tomasz Barański <tomasz.baranski@posteo.net>
2024-12-03 18:43:42 +0100

    Day 03 part 2

--- src/tomob/day03.clj
+++ src/tomob/day03.clj
@@ -3,6 +3,7 @@
             [clojure.string :as string]))
 
 (def mul-regex #"mul\((\d+),(\d+)\)")
+(def mul-do-dont-regex #"(mul\((\d+),(\d+)\)|do\(\)|don't\(\))")
 
 (defn step1 [data]
   (->> 
@@ -12,5 +13,20 @@
     (map #(* (Integer. (second %)) (Integer. (nth % 2))))
     (apply +)))
 
+(defn filter-disabled [enabled instructions]
+  (if (empty? instructions) '()
+    (let [[instruction _ a b] (first instructions)]
+      (cond
+        (= instruction "do()") (filter-disabled true (rest instructions))
+        (= instruction "don't()") (filter-disabled false (rest instructions))
+        (not enabled) (filter-disabled enabled (rest instructions))
+        :else (cons (* (Integer. a) (Integer. b)) (filter-disabled enabled (rest instructions)))))))
+
 (defn step2 [data]
-  :not-implemented)
+  (->> 
+    data
+    slurp
+    (re-seq mul-do-dont-regex)
+    (filter-disabled true)
+    (apply +)
+    ))
