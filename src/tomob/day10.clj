4f75d3a5608fced9589c0cba8b2d9c826eb796fb
Tomasz Barański <tomasz.baranski@posteo.net>
2024-12-10 19:03:52 +0100

    Day 10 part 2

--- src/tomob/day10.clj
+++ src/tomob/day10.clj
@@ -43,13 +43,43 @@
   (let [topo-map (parse-map data)
         max-x (alength (aget topo-map 0))
         max-y (alength topo-map)]
-    (println (count (find-all topo-map 0 max-x max-y)))
-    (println (count (find-all topo-map 9 max-x max-y)))
     (count
       (for [zero (find-all topo-map 0 max-x max-y)
             nine (find-all topo-map 9 max-x max-y)
             :when (reachable? topo-map zero nine max-x max-y)]
         1))))
 
+(defn count-trailhead-rating [topo-map start end max-x max-y]
+  (let [memo (atom {})
+        valid-move? (fn [[x y]]
+                     (and (>= x 0) (< x max-x)
+                          (>= y 0) (< y max-y)))
+        neighbors (fn [[x y]]
+                   (filter valid-move?
+                          [[(inc x) y] [(dec x) y]
+                           [x (inc y)] [x (dec y)]]))]
+    (letfn [(count-paths [current]
+              (if-let [cached (@memo current)]
+                cached
+                (let [[cx cy] current
+                      result (cond
+                              (= current end) 1
+                              :else
+                              (let [current-val (aget topo-map cy cx)
+                                    next-positions (->> (neighbors current)
+                                                      (filter (fn [[x y]]
+                                                              (= (aget topo-map y x)
+                                                                 (inc current-val)))))]
+                                (reduce + 0 (map count-paths next-positions))))]
+                  (swap! memo assoc current result)
+                  result)))]
+      (count-paths start))))
+
 (defn step2 [data]
-  :not-implemented)
+  (let [topo-map (parse-map data)
+        max-x (alength (aget topo-map 0))
+        max-y (alength topo-map)]
+    (apply +
+      (for [zero (find-all topo-map 0 max-x max-y)
+            nine (find-all topo-map 9 max-x max-y)]
+        (count-trailhead-rating topo-map zero nine max-x max-y)))))
