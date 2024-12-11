e5975b6cf935871548266fc8a69b74dce65211c6
Tomasz Barański <tomasz.baranski@posteo.net>
2024-12-11 20:10:00 +0100

    Day 11 part 2

--- src/tomob/day11.clj
+++ src/tomob/day11.clj
@@ -13,14 +13,34 @@
           (map #(-> % string/join Long.) (split-at half stone-str)))
         :else (* 2024 stone)))))
 
+(defn blink2 [stone-map]
+  (apply
+    merge-with +
+    (for [[stone cnt] stone-map]
+      (cond
+        (= stone "0") {"1" cnt}
+
+        (even? (count stone))
+        (apply
+          merge-with + 
+          (map (fn [half] {(-> half string/join Long. str) cnt})
+               (split-at (/ (count stone) 2) stone)))
+
+        :else {(str (* 2024 (Long. stone))) cnt}))))
+
 (defn step1 [data]
   (let [init (map #(Long. %) (string/split (slurp data) #"\s+"))]
     (->> init
       (iterate blink)
-      (drop 1) ;; First result of iterate is the initial value
-      (take 25)
-      last
+      (drop 25)
+      first
       count)))
 
 (defn step2 [data]
-  :not-implemented)
+  (let [init (into {} (map (fn [x] [x 1]) (string/split (slurp data) #"\s+")))]
+    (->> init
+      (iterate blink2)
+      (drop 75)
+      first
+      vals
+      (apply +))))
