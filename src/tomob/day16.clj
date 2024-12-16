f6bc27add6c28f7b36465cc1e4eec869d5374015
Tomasz Barański <tomasz.baranski@posteo.net>
2024-12-16 22:16:36 +0100

    Day 16 part 2

--- src/tomob/day16.clj
+++ src/tomob/day16.clj
@@ -46,6 +46,40 @@
                          ))]
         (recur nq ndist)))))
 
+(defn dijkstra-all [maze start start-dir end]
+  (loop [q (sorted-set [0 start start-dir])
+         dist {[start start-dir] 0}
+         paths {[start start-dir] #{[[start start-dir]]}}]
+    (if (empty? q) 
+      {:dist dist :paths paths}
+      (let [[d current-pos curr-dir] (first q)
+            current-key [current-pos curr-dir]
+            current-paths (get paths current-key #{})
+            [nq ndist npaths] (reduce
+                               (fn [[q d p] [next-pos next-dir alt]]
+                                 (let [next-key [next-pos next-dir]
+                                       current-alt (get d next-key Integer/MAX_VALUE)]
+                                   (cond
+                                     (< alt current-alt)
+                                     [(conj q [alt next-pos next-dir])
+                                      (assoc d next-key alt)
+                                      (assoc p next-key 
+                                        (set (map #(conj % next-key) current-paths)))]
+
+                                     (= alt current-alt)
+                                     [q d
+                                      (update p next-key into
+                                        (map #(conj % next-key) current-paths))]
+
+                                     :else
+                                     [q d p])))
+                               [(disj q (first q)) dist paths]
+                               (for [[next-pos next-dir cost] (get-next-positions maze current-pos curr-dir)
+                                     :let [alt (+ (get dist current-key Integer/MAX_VALUE) 
+                                                (step-cost curr-dir next-dir))]]
+                                 [next-pos next-dir alt]))]
+        (recur nq ndist npaths)))))
+
 (defn print-maze [maze path]
   (doseq [[[x y] dir] path]
     (case dir
@@ -58,16 +92,44 @@
         (print (aget maze y x)))
     (println)))
 
+(defn find-best [dist end]
+  (->> dist
+         (filter (fn [[[pos dir] _]] (= pos end)))
+         (map (fn [[k v]] v))
+         sort
+         first))
+
 (defn step1 [data]
   (let [maze (-> data slurp string/split-lines to-array-2d)
         start (find-pos maze \S)
         end (find-pos maze \E)
         dist (dijkstra maze start :east end)]
-    (->> dist
-         (filter (fn [[[pos dir] _]] (= pos end)))
-         (map (fn [[k v]] v))
-         sort
-         first)))
+    (find-best dist end)))
+
+(defn find-all-best [{:keys [dist paths]} end]
+  (let [best-dist (->> dist
+                      (filter (fn [[[pos _] _]] (= pos end)))
+                      (map (fn [[k v]] v))
+                      sort
+                      first)
+        best-paths (->> paths
+                       (filter (fn [[[pos _] _]] (= pos end)))
+                       (filter (fn [[k _]] (= (dist k) best-dist)))
+                       (mapcat (fn [[_ paths]] paths)))]
+    {:distance best-dist
+     :paths best-paths}))
+
+(defn get-points [path]
+  (map (fn [[pos dir]] pos) path))
 
 (defn step2 [data]
-  :not-implemented)
+  (let [maze (-> data slurp string/split-lines to-array-2d)
+        start (find-pos maze \S)
+        end (find-pos maze \E)
+        dist (dijkstra-all maze start :east end)
+        {distance :distance paths :paths} (find-all-best dist end)]
+    (->> paths
+         (map get-points)
+         (mapcat identity)
+         (into #{})
+         count)))
