d400ef3f0a95a46b8744df0c57d58c110e3a9149
Tomasz Barański <tomasz.baranski@posteo.net>
2024-12-20 18:36:11 +0100

    Day 20 part 2

--- src/tomob/day20.clj
+++ src/tomob/day20.clj
@@ -1,70 +1,80 @@
 (ns tomob.day20
   (:require [clojure.string :as string]))
 
-(defn array->map [a]
+(defn array->map [^objects a]
   (into {}
         (for [y (range (alength a))
-              x (range (alength (aget a 0)))]
+              x (range (alength ^objects (aget a 0)))]
           [[x y] (aget a y x)])))
 
-(defn find-char [m ch]
+(defn find-char [^objects m ch]
   (first 
     (for [y (range (alength m))
-          x (range (alength (aget m 0)))
+          x (range (alength ^objects (aget m 0)))
           :when (= ch (aget m y x))]
       [x y])))
 
 (defn parse-racetrack [data]
-  (let [m (->> data string/split-lines to-array-2d)]
-    [(array->map m) (find-char m \S) (find-char m \E) [(alength (aget m 0)) (alength m)]]))
+  (let [^objects m (->> data string/split-lines to-array-2d)
+        start (find-char m \S)
+        end (find-char m \E)
+        racetrack (array->map m)
+        racetrack (assoc racetrack start \. end \.)]
+    [racetrack start end [(alength ^objects (aget m 0)) (alength m)]]))
+
+(def possible-moves
+  [[-1 0] [1 0] [0 -1] [0 1]])
 
 (defn get-neighbors [[x y] racetrack]
-  (let [possible-moves [[-1 0] [1 0] [0 -1] [0 1]]]
-    (->> possible-moves
-         (map (fn [[dx dy]] [(+ x dx) (+ y dy)]))
-         (filter (fn [[nx ny]]
-                  (not= \# (racetrack [nx ny])))))))
+  (->> possible-moves
+       (map (fn [[dx dy]] [(+ x dx) (+ y dy)]))
+       (filter (fn [[nx ny]]
+                 (= \. (racetrack [nx ny]))))))
 
-(defn count-path
-  ([racetrack start end] (count-path racetrack start end Integer/MAX_VALUE))
-  ([racetrack start end max-length]
-    (loop [queue (conj clojure.lang.PersistentQueue/EMPTY [start 0])
-           visited #{start}]
-      (if (empty? queue)
-        nil
-        (let [[current steps] (peek queue)
-              queue (pop queue)]
-          (cond
-            (= current end) steps
-            (> steps max-length) nil
-            :else (let [neighbors (get-neighbors current racetrack)
-                  unvisited (remove visited neighbors)]
-              (recur
-                (into queue (map #(vector % (inc steps)) unvisited))
-                (into visited unvisited)))))))))
+(defn find-path [racetrack start end]
+  (loop [queue (conj clojure.lang.PersistentQueue/EMPTY [start [[start 0]]])
+         visited #{start}]
+    (if (empty? queue)
+      nil
+      (let [[current path] (peek queue)
+            current-length (count path)
+            queue (pop queue)]
+        (cond
+          (= current end) path
+          :else (let [neighbors (get-neighbors current racetrack)
+                      unvisited (remove visited neighbors)]
+                  (recur
+                    (into queue (map #(vector % (conj path [% current-length])) unvisited))
+                    (into visited unvisited))))))))
 
-(defn is-cheat? [racetrack [x y :as pos]]
-  (and (= \# (racetrack pos))
-       (or (and (not= \# (racetrack [(inc x) y]))
-                (not= \# (racetrack [(dec x) y])))
-           (and (not= \# (racetrack [x (inc y)]))
-                (not= \# (racetrack [x (dec y)]))))))
+(defn manhattan [[^int x1 ^int y1] [^int x2 ^int y2]]
+  (+ (abs (- x2 x1))
+     (abs (- y2 y1))))
 
-(defn find-cheats [racetrack start end [max-x max-y] at-least]
-  (let [longest (count-path racetrack start end)
-        max-length (- longest at-least)]
-    (for [x (range 1 (dec max-x))
-          y (range 1 (dec max-y))
-          :when (is-cheat? racetrack [x y])
-          :let [l (count-path (assoc racetrack [x y] \.) start end max-length)]
-          :when (not (nil? l))
-          :when (>= (- longest l) at-least)]
-      [[x y] (- longest l)])))
+(defn neighbors-within [path pos mn mx]
+  (for [[coord _ :as point] path
+        :when (>= mx (manhattan pos coord) mn)]
+    point))
+
+(defn find-cheats [racetrack start end [mn mx] at-least]
+  (let [path (find-path racetrack start end)]
+    (for [[pos length] path
+          [neighbor n-length] (neighbors-within path pos mn mx)
+          :when (> n-length length)
+          :let [saved (- n-length length (manhattan pos neighbor))]
+          :when (>= saved at-least)]
+      [pos neighbor saved])))
 
 (defn step1 [data]
   (let [[racetrack start end maxs] (parse-racetrack (slurp data))]
-    (->> (find-cheats racetrack start end maxs 100)
-         count)))
+    (->> (find-cheats racetrack start end [2 2] 100)
+         (group-by (fn [[_ _ l]] l))
+         (map (fn [[k v]] (count v)))
+         (reduce +))))
 
 (defn step2 [data]
-  :not-implemented)
+  (let [[racetrack start end maxs] (parse-racetrack (slurp data))]
+    (->> (find-cheats racetrack start end [2 20] 100)
+         (group-by (fn [[_ _ l]] l))
+         (map (fn [[k v]] (count v)))
+         (reduce +))))
